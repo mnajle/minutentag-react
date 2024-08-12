cf9c7e3e763478dbb1b349cedb419487d4be6183
Maximiliano Najle <maximilianonajle@gmail.com>
2024-08-12 16:40:39 -0300

    Commit code

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import api from "./src/api.js"
 
 const app = express()
-const port = 3000
+const port = 5000
 
 
 app.use(cors())
---
+++ backend/readme.md
@@ -0,0 +1,3 @@
+# Instructions to run
+1. npm install
+2. npm start
